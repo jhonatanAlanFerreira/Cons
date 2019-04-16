856270c5f566f3d781d89502ee795b47739c4d1f
Jhonatan <Jhonatan.alanf@gmail.com>
2019-04-15 21:18:01 -0300

    getId

---
+++ getId.ts
@@ -0,0 +1,9 @@
+ngOnInit() {
+    this.route.params
+      .subscribe(
+        (params: Params) => {
+          this.id = +params['id'];
+          this.recipe = this.recipeService.getRecipe(this.id);
+        }
+      );
+  }
